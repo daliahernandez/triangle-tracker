073468841baad5467c5dd87a527a5dde0ea59980
dalia hernandez <daliahdzr98@gmail.com>
2018-12-23 18:31:11 -0800

    add input in html them write three global varbles. write your first if statements that ask you to fill in the blanks to get an accurate answer. write second if statment tthat determins if the user inputs equal each other, if it does then it's a equalateral. write and else if and determine if the user input dont equal each other then its a scalene. write an if and else if statments that determine id two sides equal each other and the other doesnt then its a isosceles. lastly, write and if statments that determines if the sum of the first two sides are less than or eequal to the thied side, then its not a triangle.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('form#userAnswer').submit(function(event){
+    var sideOne = $('input#sideOne').val();
+    var sideTwo = $('input#sideTwo').val();
+    var sideThree = $('input#sideThree').val();
+    var result = sideOne === sideTwo;
+    var resultTwo = sideOne === sideThree;
+    var resultThree = sideTwo === sideThree;
+
+    if(!sideOne || !sideTwo || !sideThree){
+      alert("fill in the blanks to get an accurate answer")
+    }
+
+    if(sideOne === sideTwo || sideOne === sideThree || sideTwo === sideThree){
+      $('#type').text("it's an Equilateral")
+    }else if(sideOne !== sideTwo || sideOne !== sideThree || sideTwo !== sideThree){
+      $('#type').text("it's a scalene")
+    }
+    if(result !== sideThree){
+      $('#type').text('its and isosceles');
+    }else if(resultTwo !== sideTwo){
+      $('#type').text('its and isosceles');
+    }else if (resultThree !== sideOne)
+    if(sideOne + sideTwo <= sideThree){
+      $('#type').text('not a Triangle')
+    }
+
+    event.preventDefault();
+
+  });
+});
